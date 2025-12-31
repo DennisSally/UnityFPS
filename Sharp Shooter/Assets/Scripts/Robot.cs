f96bbd231d0b5465da9766f1daf3cba31dd0270b
DennisSally <56869210+DennisSally@users.noreply.github.com>
2025-12-31 17:54:47 +0000

    Creating Project

---
+++ Sharp Shooter/Assets/Scripts/Robot.cs
@@ -0,0 +1,22 @@
+using StarterAssets;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Robot : MonoBehaviour
+{
+    FirstPersonController player;
+    NavMeshAgent agent;
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+    private void Start()
+    {
+        player = FindFirstObjectByType<FirstPersonController>();
+
+    }
+    private void Update()
+    {
+        agent.SetDestination(player.transform.position);
+    }
+}
