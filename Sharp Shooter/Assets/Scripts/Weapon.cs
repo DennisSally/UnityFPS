9321c818f2459e659a45afa9771ee11fd4a336cd
DennisSally <56869210+DennisSally@users.noreply.github.com>
2026-01-02 22:06:22 +0000

    Section 6.10
    Introduced Idle and Shoot animations for the weapon, created a Weapon animator controller, and added a new child game object "Active Weapon" for the Pistol and future weapons to be parented too.
    Modified Weapon.cs to trigger the Shoot animation when firing. Updated Main.unity to reference the new animation setup.

--- Sharp Shooter/Assets/Scripts/Weapon.cs
+++ Sharp Shooter/Assets/Scripts/Weapon.cs
@@ -6,11 +6,15 @@ public class Weapon : MonoBehaviour
     [SerializeField] int damageAmount = 1;
     [SerializeField] ParticleSystem muzzleFlash;
 
+    const string SHOOT_STRING = "Shoot";
+
     StarterAssetsInputs starterAssetsInputs;
+    Animator animator;
 
     private void Awake()
     {
         starterAssetsInputs = GetComponentInParent<StarterAssetsInputs>();
+        animator = GetComponentInParent<Animator>();
     }
 
     // Update is called once per frame
@@ -23,13 +27,14 @@ public class Weapon : MonoBehaviour
     {
         if (!starterAssetsInputs.shoot) return;
         muzzleFlash.Play();
+        animator.Play(SHOOT_STRING, 0, 0f);
+        starterAssetsInputs.ShootInput(false);
+        
         RaycastHit hit;
-
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
         {
             EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
             enemyHealth?.TakeDamage(damageAmount);
         }
-        starterAssetsInputs.ShootInput(false);
     }
 }
